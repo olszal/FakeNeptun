166911dc0b9bd17305060ff13411c7487de06c84
DESKTOP-9IKT19D\User <ratky.laszlo@gmail.com>
2024-12-18 14:25:04 +0100

    Bemutatás előtt

--- client/src/app/component/course/add-course/add-course.component.ts
+++ client/src/app/component/course/add-course/add-course.component.ts
@@ -1,23 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CourseFormComponent } from '../course-form/course-form.component';
 import { Course } from '../../../model/course';
+import { LoginComponent } from '../../login/login/login.component';
 import { CourseService } from '../../../service/course.service';
 import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../../service/authservice';
+import { User } from '../../../model/user';
+import { Teacher } from '../../../model/teacher';
 
 @Component({
   selector: 'app-add-course',
   standalone: true,
-  imports: [ CourseFormComponent, MatCardModule ],
+  imports: [ CourseFormComponent, MatCardModule, LoginComponent ],
   templateUrl: './add-course.template.html',
   styles: ``,
 })
 export class AddCourseComponent {
 
-  constructor(private router: Router, private courseService: CourseService) {}
+  constructor(private router: Router, private courseService: CourseService, private authService: AuthService) {}
 
   addCourse(course: Course) {
-    this.courseService.createCourse(course).subscribe({
+    //if (!this.authService.isLoggedIn()) return;
+    this.courseService.createCourse(course, this.authService.getUser()).subscribe({
       next: () => {
         this.router.navigate(['/courses']);
       },
